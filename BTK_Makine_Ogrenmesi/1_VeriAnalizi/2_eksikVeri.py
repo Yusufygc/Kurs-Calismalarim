32cfde91d83b151949f38e406565e058ca3a7cbf
Yusufygc <yusufygc118@gmail.com>
2025-07-22 15:08:54 +0300

    eksik veri ile calismak

--- BTK_Makine_Ogrenmesi/1_VeriAnalizi/2_eksikVeri.py
+++ BTK_Makine_Ogrenmesi/1_VeriAnalizi/2_eksikVeri.py
@@ -6,6 +6,7 @@ Created on Tue Jul 22 13:19:13 2025
 """
 
 import pandas as pd
+import numpy as np
 from sklearn.impute import SimpleImputer
 
 
@@ -14,4 +15,17 @@ data = pd.read_csv("eksikveri.csv")
 df = pd.DataFrame(data)
 
 print(df)
+
+imputer = SimpleImputer(missing_values=np.nan,strategy='mean') # nan olan degererin yerine o sutunun ortalama degerini yazcaz
+
+Yas = df.iloc[:,1:4].values
+print(Yas)# yas boy kilo verisi beraber geldi
+
+imputer = imputer.fit(Yas[:,1:4]) # burda ortalama veriyi ogrendi imputer eksik verilerin yerine koyulacak degeri
+Yas[:,1:4] = imputer.transform(Yas[:,1:4]) # yas taki nan degerler degisecek
+
+print(Yas)
+
+
+
 
