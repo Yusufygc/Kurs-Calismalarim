1f70d25954faee8bf153b15c39075ffd312b2ef5
Yusufygc <yusufygc118@gmail.com>
2025-07-25 15:33:10 +0300

    Coklu Lineer Regresyon \ Veri hazirlama kismi yapildi

---
+++ BTK_Makine_Ogrenmesi/2_Tahmin/2_CokluLineerRegresyon/1_CokluLineerRegresyon.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.impute import SimpleImputer
+from sklearn import preprocessing
+
+data = pd.read_csv("veriler.csv")
+
+df = pd.DataFrame(data)
+
+Yas = df.iloc[:,1:4].values
+#---------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------#
+
+# ULKELER ICIN ENCODING ISLEMI
+
+ulke = df.iloc[:,0:1].values #İlk sütunu alır, NumPy 2D array'e çevirir
+#print(ulke[:,0]) #	Tüm ülke değerlerini 1D NumPy array olarak verir
+
+labelEnocoder = preprocessing.LabelEncoder()
+ulke[:,0] = labelEnocoder.fit_transform(df.iloc[:,0])
+
+oneHotEncoder = preprocessing.OneHotEncoder()
+ulke = oneHotEncoder.fit_transform(ulke).toarray()
+
+#---------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------#
+
+# CINSIYET ICIN ENCODING ISLEMI
+
+cinsiyet = df.iloc[:,-1:].values #İlk sütunu alır, NumPy 2D array'e çevirir
+#print(ulke[:,0]) #	Tüm ülke değerlerini 1D NumPy array olarak verir
+
+labelEnocoder = preprocessing.LabelEncoder()
+cinsiyet[:,-1] = labelEnocoder.fit_transform(df.iloc[:,-1])
+
+oneHotEncoder = preprocessing.OneHotEncoder()
+cinsiyet = oneHotEncoder.fit_transform(cinsiyet).toarray()
+
+#---------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------#
+
+# VERILERIN BIRLESTIRILIP DATAFRAME OLUSTURULMASI
+sonuc = pd.DataFrame(data=ulke, index=range(22), columns=['fr','tr','us'])
+
+yenidf = pd.DataFrame(Yas, index=range(22), columns=['boy','kilo','yas'])
+
+birlestirilmisDf = pd.concat([sonuc,yenidf], axis=1)
+
+
+cinsiyetDf =pd.DataFrame(cinsiyet[:,:1], index=range(22), columns=['cinsiyet'])
+
+birlestirilmisTamDf = pd.concat([birlestirilmisDf,cinsiyetDf], axis=1)
+print(birlestirilmisTamDf)
+
+#---------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------#
+
+# VERILERIN TEST VE TRAIN OLARAK BOLUNMESI
+
+from sklearn.model_selection  import train_test_split
+
+x_train,x_test, y_train, y_test = train_test_split(birlestirilmisDf,cinsiyetDf,test_size=0.33,random_state=0)
+
+#---------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------#
+
+# OZNITELIK OLCEKLEME
+
+from sklearn.preprocessing import StandardScaler
+
+sc =StandardScaler()
+
+X_train =sc.fit_transform(x_train)
+X_test =sc.fit_transform(x_test)
+
+
+
+
+
+
