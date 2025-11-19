e79eab95cd1c5a2d65822aaa50f1effd7c03bf0f
Yusufygc <yusufygc118@gmail.com>
2025-11-19 23:59:18 +0300

    KNN Makine Ogrenmesi Uygulamalari

---
+++ BTK_Makine_Ogrenmesi_Uygulamalari/1_Gozetimli_ML/1_KNN/1_knn_pratigi.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+cancer = load_breast_cancer()
+df =pd.DataFrame(data=cancer.data, columns=cancer.feature_names)
+df['target'] = cancer.target
