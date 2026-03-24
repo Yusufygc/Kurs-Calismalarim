5e689d6334257ef4a83a236acb45fce62687bb2e
Yusufygc <yusufygc118@gmail.com>
2026-03-24 11:25:43 +0300

    Knn pratiği

--- BTK_Makine_Ogrenmesi_Uygulamalari/1_Gozetimli_ML/1_KNN/1_knn_pratigi.py
+++ BTK_Makine_Ogrenmesi_Uygulamalari/1_Gozetimli_ML/1_KNN/1_knn_pratigi.py
@@ -6,3 +6,5 @@ from sklearn.model_selection import train_test_split
 cancer = load_breast_cancer()
 df =pd.DataFrame(data=cancer.data, columns=cancer.feature_names)
 df['target'] = cancer.target
+
+knn = KNeighborsClassifier(n_neighbors=3)
